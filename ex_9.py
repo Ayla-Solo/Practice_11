0f5631f8556a71d81f62ca415eb8a4f6f3bfae33
Ayla-Solo <o.soloveva2@g.nsu.ru>
2026-04-18 13:26:21 +0700

    ex_9.py

--- ex_9.py
+++ ex_9.py
@@ -1,22 +1,27 @@
-mass_text = []
-text = ""
-quantity = []
-no_repetitions_mass = []
+
+mass_text = []  # список слов из текущей строки
+text = ""       # вводимая строка
+quantity = []   # количество повторений слов
+no_repetitions_mass = []  # список уникальных слов
+
 
 while text != " ":
     text = input()
-    text = text.replace(",", "")
-    text = text.replace(".", "")
-    text = text.lower()
+    text = text.replace(",", "")  
+    text = text.replace(".", "")  
+    text = text.lower()     
     if text != " ":
-        mass_text = list(text.split())
+        mass_text = list(text.split())  # разбиваем строку на слова
         for i in range(len(mass_text)):
             if mass_text[i] in no_repetitions_mass:
+                # увеличиваем счетчик слова
                 quantity[no_repetitions_mass.index(mass_text[i])] += 1
             else:
+                # добавляем новое слово
                 no_repetitions_mass.append(mass_text[i])
                 quantity.append(1)
 
+# сортировка по убыванию частоты 
 for i in range(len(quantity)-1):
     for j in range(len(quantity)-i-1):
         if quantity[j] < quantity[j+1]:
